291c45c4d35e176ac18e1224bbc699678f454991
Dominic Magnifico <dominicmagnifico@gmail.com>
2016-02-02 23:30:09 -0700

    Remove offline version and just load the home page.

--- _site/serviceworker.js
+++ _site/serviceworker.js
@@ -2,8 +2,10 @@ this.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches.open('offline').then(function(cache) {
 			return cache.addAll([
-				'/offline/',
-				'/offline/index.html',
+				'/',
+				'/index.html',
+				'/css',
+				'/css/main.css',
 				'/app.js',
 				'/serviceworker.js'
 			]);
