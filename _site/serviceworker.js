b02d3d5fe86c0d5c2181e02359a3219259ed2b49
Dominic Magnifico <dominicmagnifico@gmail.com>
2016-02-02 23:14:09 -0700

    Add serviceworker file.

---
+++ _site/serviceworker.js
@@ -0,0 +1,12 @@
+this.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open('offline').then(function(cache) {
+			return cache.addAll([
+				'/offline/',
+				'/offline/index.html',
+				'/app.js',
+				'/serviceworker.js'
+			]);
+		})
+	);
+});
