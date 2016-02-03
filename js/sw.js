557baa614d6e551aa6d5ae4c92673da8236626ca
Dominic Magnifico <dominicmagnifico@gmail.com>
2016-02-02 22:52:35 -0700

    Add app and sw js files to the cache.

--- js/sw.js
+++ js/sw.js
@@ -2,8 +2,10 @@ this.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('offline').then(function(cache) {
       return cache.addAll([
-        '/offline/',
-        '/offline/index.html'
+				'/offline/',
+        '/offline/index.html',
+				'/js/app.js',
+				'/js/sw.js'
       ]);
     })
   );
