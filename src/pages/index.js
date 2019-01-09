ed3e7d1cc78661e1f2d5fa15bf43be194f493dfa
Dominic Magnifico <dominicmagnifico@gmail.com>
2019-01-09 00:27:12 -0700

    Start Gatsby move.

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Header from '../components/Header';
+
+export default () => (
+  <>
+    <Header />
+    <section className="cf pv6 ph4 pa5-l bt mw9 center">
+      <article class="pb4 pt0 bt bb b--black-10 ph3 ph0-l">
+        <div class="db">
+          <div class="w-100">
+            <h1 class="f3 f1-l m0 lh-title">Tech Giant Invests Huge Money to Build a Computer Out of Science Fiction</h1>
+          </div>
+        </div>
+        <time class="f5 db gray">Nov. 21, 2016</time>
+      </article>
+    </section>
+  </>
+)
