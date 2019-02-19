56fb22bcee957d2161570c13f9e816e217ff0ecb
Dominic Magnifico <dominicmagnifico@gmail.com>
2019-02-18 18:20:31 -0700

    Updates

--- _site/app.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/serviceworker.js').then(function(reg) {
-		console.log('Registration succeeded.');
-	}).catch(function(error) {
-		console.log('Registration failed with ' + error);
-	});
-}
