4b0bfc3c3c6e9455e5aebbd47f4915330b3d06c4
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-05-13 10:58:45 -0600

    Remove theme switcher.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import Logo from './Logo';
-import { useTheme } from '../ThemeContext';
+// import { useTheme } from '../ThemeContext';
 
 import 'tachyons';
 
-const Header = ({ children  }) => {
-  const {dark, themeToggle} = useTheme();
+const Header = () => {
+  // const {dark, themeToggle} = useTheme();
 
   return (
     <nav className="primary-nav bg-parent db dt-l fixed w-100 border-box pa3 ph5-l avenir">
@@ -14,7 +14,7 @@ const Header = ({ children  }) => {
         <Logo width="30" />
       </Link>
       <div className="db f6 dtc-l v-mid w-100 w-25-l tc ttu tr-l">
-        <button className={`btn ${dark ? '-light' : '-dark'}`} onClick={() => themeToggle()}>{dark ? 'Light' : 'Dark'}</button>
+        {/* <button className={`btn ${dark ? '-light' : '-dark'}`} onClick={() => themeToggle()}>{dark ? 'Light' : 'Dark'}</button> */}
         <Link to="/about" className="link dim dib mr3">About</Link>
         {/* <Link to="/work" className="link dim white dib mr3">Work</Link> */}
         <Link to="/resume" className="link dim dib">R&eacute;sum&eacute;</Link>
