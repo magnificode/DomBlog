6c1a69ab03175a4ab25deb5376ba773d3f9aa77a
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-05-13 10:35:12 -0600

    Remove theme context provider.

--- src/templates/wrapper.js
+++ src/templates/wrapper.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
-import { ThemeProvider, ThemeContext } from '../ThemeContext';
+// import { ThemeProvider, ThemeContext } from '../ThemeContext';
 
 const Wrapper = ( { children } ) => {
 
   return (
-    <ThemeProvider>
-      <ThemeContext.Consumer>
-        {(context) => (
-          <div className={`wrapper ${context.dark ? '-dark' : '-light'}`}>
-            <Header />
-            {children}
-          </div>
-        )}
-      </ThemeContext.Consumer>
-    </ThemeProvider>
+    <div className={`wrapper`}>
+      <Header />
+      {children}
+    </div>
   );
 }
 
