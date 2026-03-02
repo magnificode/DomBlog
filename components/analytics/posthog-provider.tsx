0ed015a6c32b42d4f6fa7ba7b5ad18558da71c06
Dominic Magnifico <dom@houseofgiants.com>
2026-03-02 15:55:26 -0700

    remove phosthog keys

--- components/analytics/posthog-provider.tsx
+++ components/analytics/posthog-provider.tsx
@@ -6,7 +6,7 @@ import posthogClient from 'posthog-js';
 import { PostHogProvider as PostHogProviderClient } from 'posthog-js/react';
 import { PostHogPageview } from '@/components/analytics/posthog-pageview';
 
-const POSTHOG_KEY = process.env.NEXT_PUBLIC_POSTHOG_KEY ?? 'phc_dg0ZjS4fdvcQ92hlGYFhqGTCIhjJ2q7tg6jwdgzN0Uw';
+const POSTHOG_KEY = process.env.NEXT_PUBLIC_POSTHOG_KEY;
 const POSTHOG_HOST = process.env.NEXT_PUBLIC_POSTHOG_HOST ?? 'https://us.i.posthog.com';
 
 let initialized = false;
@@ -14,6 +14,7 @@ let initialized = false;
 export function PostHogProvider({ children }: Readonly<{ children: React.ReactNode }>) {
 	useEffect(() => {
 		if (initialized) return;
+		if (!POSTHOG_KEY) return;
 
 		posthogClient.init(POSTHOG_KEY, {
 			api_host: POSTHOG_HOST,
